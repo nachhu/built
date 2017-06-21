ed460407faafa2433ed77fbaae46c8b354185450
nachhu <nachhu08@gmail.com>
2017-06-21 08:26:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var http = require('http');
-
+var port = process.env.PORT||8080;
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
-}).listen(8080);
+}).listen(port);
 
 console.log('Server started');
